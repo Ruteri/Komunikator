eb9a01fa4a686f4baf9645e03d375e0e7c6f0cf3
Ruteri <mmorusiewicz@gmail.com>
2015-03-17 16:34:20 +0100

    MM V0

---
+++ User.cpp
@@ -0,0 +1,9 @@
+//
+//  User.cpp
+//  KomunikatorV2
+//
+//  Created by Konto Mateusza on 17.03.2015.
+//  Copyright (c) 2015 Mateusz. All rights reserved.
+//
+
+#include "User.h"
---
+++ User.h
@@ -0,0 +1,14 @@
+//
+//  User.h
+//  KomunikatorV2
+//
+//  Created by Konto Mateusza on 17.03.2015.
+//  Copyright (c) 2015 Mateusz. All rights reserved.
+//
+
+#ifndef __KomunikatorV2__User__
+#define __KomunikatorV2__User__
+
+#include <stdio.h>
+
+#endif /* defined(__KomunikatorV2__User__) */
