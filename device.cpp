73e3a13576e331a72223cf4cfe3ae19875dc5918
bopazyn <arkadiusz.swierczek@gmail.com>
2015-03-21 18:44:47 +0100

    test

--- device.cpp
+++ device.cpp
@@ -1,3 +1,73 @@
 // Arkadiusz Œwierczek
 
-#include"device.h"
+#include "Device.h"
+
+device::device()
+{
+	active = false;
+}
+device::device(bool i)
+{
+	active = i;
+}
+device::~device()
+{
+	if (!functions.empty())
+		functions.clear();
+}
+bool device::isActive()
+{
+	return active ? 1 : 0;
+}
+void device::activate()
+{
+	active = true;
+}
+void device::deActivate()
+{
+	active = false;
+}
+void device::addFunction(int i, std::string nazwa)
+{
+	functions[i]=nazwa;
+}
+void device::addFunction(std::string nazwa)
+{
+	int size = functions.size();
+	functions[size] = nazwa;
+}
+void device::deleteFunction(int i)
+{
+	iterator = functions.find(i);
+	if (iterator->first!=0)
+		functions.erase(iterator);
+}
+void device::deleteFunction(std::string nazwa)
+{
+	for (iterator = functions.begin(); iterator != functions.end(); ++iterator)
+	{
+		if (iterator->second == nazwa)
+		{
+			functions.erase(iterator);
+			return;
+		}
+	}
+}
+std::string device::showFunctions()
+{
+	std::string lista;
+	if (functions.empty()) return "No functions added\n";
+	for (iterator = functions.begin(); iterator != functions.end(); ++iterator)
+	{
+		lista += std::to_string(iterator->first);
+		lista += '\t';
+		lista += iterator->second;
+		lista += '\n';
+	}
+	return lista;
+}
+std::map<int, std::string> *device::showFunctions2()
+{
+	if (functions.empty()) return nullptr;
+	return &functions;
+}
--- device.h
+++ device.h
@@ -1,8 +1,35 @@
 #ifndef DEVICE_H
 #define DEVICE_H
 
- // klasa funkcjonalnosc!
-class device{
-	
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+#include "ComSubject.h"
+
+class device : public ComSubject{
+private:
+	bool active;
+	std::map<int, std::string> functions;
+	std::map<int, std::string>::iterator iterator;
+	std::vector <std::string> history;
+
+
+public:
+	device();
+	device(bool);
+	~device();
+	bool isActive();
+	void activate();
+	void deActivate();
+	std::string showFunctions();
+	std::map<int, std::string> *showFunctions2();
+	void addFunction(int, std::string);
+	void addFunction(std::string);
+	void deleteFunction(int);
+	void deleteFunction(std::string);
+	std::string showLastOperations();
+	std::vector<std::string> *showLastOperations2();
 };
-#endif
+#endif /* defined(__KomunikatorV2__Device__) */
