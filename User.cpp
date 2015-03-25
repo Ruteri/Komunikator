84b03ac90d7eca288331e258ec9c0e4078f34363
Ruteri <mmorusiewicz@gmail.com>
2015-03-25 19:03:36 +0100

    Git malina. @Macie: Nie dziala jesli w Message.g nie zmienisz funckji na const {}

--- User.cpp
+++ User.cpp
@@ -6,6 +6,8 @@ User::User(int Id): ComSubject(Id), listaPrzyjaciol(new std::vector<ComSubject*>
 
 bool User::addFriend(ComSubject* toAdd)
 {
+    if (checkIfInFriends(toAdd)) return 1;
+    
     listaPrzyjaciol->push_back(new ComSubject(toAdd->getID()));
     return 0;
 }
@@ -25,7 +27,7 @@ bool User::removeFriend(ComSubject* toDel)
     return 1;
 }
 
-std::vector<ComSubject*>::const_iterator User::getIterator()
+std::vector<ComSubject*>::const_iterator User::getFriendsListIterator()
 {
     return listaPrzyjaciol->cbegin();
 }
@@ -37,10 +39,26 @@ bool User::checkIfInFriends(ComSubject* toFind)
     for (;iterator!=listaPrzyjaciol->end();++iterator)
     {
         if (*iterator == toFind) {
-            return 0;
+            return true;
         }
     }
     
-    return 1;
+    return false;
 
+}
+
+std::vector<const Message*>* User::findMessagesWith(ComSubject* toFind)
+{
+    int i;
+    
+    std::vector<const Message*>* vect = new std::vector<const Message*>;
+    
+    for (i = 0; i < MessageListSize(); ++i) {
+        if (getMessage(i)->getReciever()->getID() == toFind->getID() || getMessage(i)->getSender()->getID() == toFind->getID()) {
+            vect->push_back(getMessage(i));
+        }
+    }
+    
+    return vect;
+    
 }
--- User.h
+++ User.h
@@ -3,7 +3,7 @@
 //  KomunikatorV2
 //
 //  Created by Konto Mateusza on 17.03.2015.
-//  Copyright (c) 2015 Mateusz. All rights reserved.
+//  Copyright (c) 2015 Mateusz. All rights reserv.
 //
 
 #ifndef __KomunikatorV2__User__
@@ -27,10 +27,29 @@ public:
     bool addFriend(ComSubject*);
     bool removeFriend(ComSubject*);
     
-    std::vector<ComSubject*>::const_iterator getIterator();
+    std::vector<ComSubject*>::const_iterator getFriendsListIterator();
     
     bool checkIfInFriends(ComSubject*);
     
+    std::vector<const Message*>* findMessagesWith(ComSubject*);
+    
+    friend std::ostream& operator<<(std::ostream& stream, User& user)
+    {
+        
+        auto iterator = user.listaPrzyjaciol->begin();
+       
+        stream << "User with ID: " << user.getID() << std::endl << "Friends: " << std::endl;
+        for (;iterator != user.listaPrzyjaciol->end() ; ++iterator) {
+            stream << "ID: " << (*iterator)->getID() << std::endl << "Messages: " << std::endl;
+            auto listaWiadomosci = user.findMessagesWith(*iterator);
+            for (int i = 0; i < listaWiadomosci->size(); ++i) {
+                stream << (*listaWiadomosci)[i]->getText() << std::endl;
+            }
+            stream << std::endl;
+        }
+        
+        return stream;
+    }
     
 };
 
